af5261f29b56b93fbe1f4e0f5068d9f9738041f7
tai2 <hello@tai2.net>
2023-11-05 10:11:13 +0900

    decor 1.0.4

--- Formula/decor.rb
+++ Formula/decor.rb
@@ -1,8 +1,8 @@
 class Decor < Formula
   desc "Markdown transformer for single page, which uses a HTML file as a template"
   homepage "https://github.com/tai2/decor"
-  url "https://github.com/tai2/decor/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "d4563a8f743c581e57c141ecbda58bc3c5b7d58aaca027d89f00907865275bef"
+  url "https://github.com/tai2/decor/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "f254d2687dc51c4f41ddc6bd8cf8df4db99e2bb97c627625b669b941ae419676"
   license "MIT"
 
   depends_on "deno"
